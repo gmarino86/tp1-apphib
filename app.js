1c2ebde8e469b283a6340320b711820f44b82536
Gaston Marino <gmarino86@gmail.com>
2022-05-13 20:59:03 -0300

    Base

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from 'express'
 import AlumnosRoute from './routes/alumnos.route.js'
-const app = express() // crea el objeto de la aplicacion
+const app = express()
 
 app.set('view engine', 'ejs')
 
 app.use('/', express.static('public'))
-app.use(express.urlencoded({ extended: false }))// ParseBody cuando viene en formato urlencoded
-app.use(express.json()) // ParseBody cuando viene en formato JSON
-app.use('/', AlumnosRoute)
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json()) 
 
+/* RUTAS */
+app.use('/', AlumnosRoute)
 app.get('/ruta/prueba', function (req, res) {
   res.render('hola', { titulo: '<strong>Hola</strong>', mostrar: true })
 })
 
-// Escucha el puerto y si funcioa, ejecuta la funcion
+
+/* SERVER EXPRESS */
 app.listen(2022, function () {
-  console.log('Me conecte al puerto! http://localhost:2022')
+  console.log('Hola! http://localhost:2022')
 })
